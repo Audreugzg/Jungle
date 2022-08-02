c8ee753212cf47769f57860137db1a263b3ecc74
Zuge Gao <audreygaozu@hotmail.com>
2022-08-02 21:27:47 +0000

    feature/user-specs done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
+  
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true
+  validates :password, length: { minimum: 3 }
+  validates :password_confirmation, length: { minimum: 3 }
+  validates :email, uniqueness: { case_sensitive: false }
+  before_save { email.downcase! }
   has_secure_password
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email)
+
+    if @user && @user.authenticate(password)
+      return @user
+    else
+      return nil
+    end
+  end
 end
